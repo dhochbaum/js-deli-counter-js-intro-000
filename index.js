05b3fcf439850f7b790063680d1d4739c62cffd1
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 23:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function nowServing(katzDeliLine) {
     return `Currently serving ${katzDeliLine.shift()}.`
   }
 }
+
+function currentLine(line) {
+  if(line.length===0) {
+    return "The line is currently empty."
+  } else {
+    var str = "The line is currently: "
+    for (let i = 0; i < line.length; i++) {
+      str = str + `${i === 0 ? '' : ', '}${i+1}. ${line[i]}`
+    }
+    return str
+  }
+}
